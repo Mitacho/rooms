65cba46e5ec459082810aaca9e277f2f944fce60
Mitacho <arthurf@tuta.io>
2022-03-13 21:53:17 -0300

    feat(Login): redirect if user is logged in

--- web/src/pages/login.tsx
+++ web/src/pages/login.tsx
@@ -1,9 +1,18 @@
 import { Form, Layout } from "components";
+import { useMeQuery } from "generated/graphql";
 import { NextPage } from "next";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import { Fragment } from "react";
 
 const Login: NextPage = () => {
+  const router = useRouter();
+  const { data } = useMeQuery();
+
+  if (data?.me) {
+    router.replace("/");
+  }
+
   return (
     <Fragment>
       <Head>
